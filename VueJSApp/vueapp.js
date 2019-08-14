8fe42ce162239b7a8b4704d98f92da8048da2aa1
oscar <Bidmlecdctr03>
2019-08-14 18:02:56 -0500

    audi s3 update db + vue app now has cards for cars

--- VueJSApp/vueapp.js
+++ VueJSApp/vueapp.js
@@ -6,7 +6,7 @@ var BrandsComponent = Vue.component('brands', {
         }
     },
     template: `
-        <div class="container">
+        <div class="container mt-md-5 mb-md-5">
             <div class="row">
                 <div class="col">
                     <div v-if="loading" class="spinner-border" role="status">
@@ -15,12 +15,12 @@ var BrandsComponent = Vue.component('brands', {
                     <template v-if="!loading">
                         <h3 v-if="brands.length === 0">There are no brands</h3>
                         <div v-else>
-                            <ul>
-                                <li v-for="brand in brands">
+                            <div class="row">
+                                <div class="col-md-3" v-for="brand in brands">
                                     {{ brand.name }}
                                     <img :src="brand.logo" class="brand-logo" alt="">
-                                </li>
-                            </ul>
+                                </div>
+                            </div>
                         </div>
                     </template>                    
                 </div>
@@ -59,17 +59,17 @@ var CarsComponent = Vue.component('cars', {
                         <h3 v-if="cars.length === 0">There are no cars</h3>
                         <div v-else>
                             <div class="row">
-                                <div class="col" v-for="car in cars">
+                                <div class="col-md-4" v-for="car in cars">
                                     <div class="card">
                                       <img :src="car.image" class="card-img-top" alt="...">
                                       <div class="card-body">
                                         <h5 class="card-title">{{ car.model }}</h5>
-                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                        <p class="card-text">{{ car.brand }}</p>
                                       </div>
                                       <ul class="list-group list-group-flush">
-                                        <li class="list-group-item">{{ car.performance.0to100 }}</li>
-                                        <li class="list-group-item">{{ car.engine.horsepower }}</li>
-                                        <li class="list-group-item">{{ car.engine.displacement }}</li>
+                                        <li class="list-group-item">0-100 km/h: {{ car.performance.zeroToAHundred }}</li>
+                                        <li class="list-group-item">Horsepower: {{ car.engine.horsepower }}</li>
+                                        <li class="list-group-item">Engine displacement: {{ car.engine.displacement }}</li>
                                       </ul>
                                     </div>
                                 </div>                        
